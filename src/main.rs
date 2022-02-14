d0b6d51e3883865245cebab475cd24800fd26dc7
Sourav Paul <psour@amazon.com>
2022-02-14 19:36:30 +0000

    Add loop to sleep and print continuously.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
+
 fn main() {
     println!("Hello, world!");
+
+    loop {
+        let now = SystemTime::now();
+
+        sleep(Duration::new(2, 0));
+        match now.elapsed() {
+            Ok(elapsed) => {
+                println!("Slept for {} seconds.", elapsed.as_secs());
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+        println!("")
+    }
 }
